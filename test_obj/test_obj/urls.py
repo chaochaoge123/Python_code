0b288e78965ef04c3a2880445f7cae71ff3b2681
qqc <qqcqqc_123@163.com>
2019-11-26 14:31:11 +0800

    "更改"

--- test_obj/test_obj/urls.py
+++ test_obj/test_obj/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from test_one import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create/signals/', views.create_signal),
